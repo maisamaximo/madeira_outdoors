0ae40a06d3fcc22cf16561c2f4d5be469ef9d86c
Maisa Maximo Ferreira <ferreira.maximo.maisa@gmail.com>
2024-03-10 01:02:11 +0000

    .

--- script/script.js
+++ script/script.js
@@ -1,19 +1,40 @@
-// window.addEventListener('scroll', function () {
-//   header.classList.toggle('sticky', window.scrollY > 60);
-// });
+const header = document.querySelector('header');
 
-// document.getElementById('menu-icon').addEventListener('click', function () {
-//   var navbar = document.querySelector('.navbar');
-//   navbar.classList.toggle('active');
-// });
-
-const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-header.classList.toggle("sticky", window.scrollY > 60)
+window.addEventListener('scroll', function () {
+  header.classList.toggle('sticky', window.scrollY > 60);
 });
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
 menu.classList.toggle('bx-x');
 navbar.classList.toggle('open');
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    var accButtons = document.querySelectorAll('.accordion-button');
+  
+    accButtons.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        // Toggle active class on the button
+        this.classList.toggle('active');
+        
+        // Control the accordion content visibility
+        var content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+        } else {
+          content.style.display = 'block';
+        }
+  
+        // Close other accordion items
+        accButtons.forEach(function (otherBtn) {
+          if (otherBtn !== btn) {
+            otherBtn.classList.remove('active');
+            otherBtn.nextElementSibling.style.display = 'none';
+          }
+        });
+      });
+    });
+  });
+  
